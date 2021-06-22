49fb6fd3686779878db9094097b775383720e8c6
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-06-22 10:14:02 +0100

    Add project title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  // Configure project name:
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Sleep Diary Dashboard';
+        return args;
+      })
+  },
+  // Ignore hidden files:
+  configureWebpack: {
+    devServer: {
+      watchOptions: {
+        ignored: [/\/\./],
+      },
+    },
+  },
   transpileDependencies: [
     'vuetify'
   ]
