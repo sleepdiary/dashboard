65bfb246595fe2c4f9f7e3a6247ef4c6d836854c
quinn-dev <quinn-dev@tuta.io>
2021-07-31 05:33:09 +0000

    Add all Roboto font weights

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,12 @@
 
 <script>
 
-import "@fontsource/roboto";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
 
 import router from "./router/index.js";
 
