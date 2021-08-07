0c29a8caa0a011338a7651d5d263734496a2d35c
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-08-07 16:19:59 +0100

    Convert entrypoint.sh to run.sh for the new build system

---
+++ bin/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ -e /opt/sleepdiary/utils.sh ]
+then . /opt/sleepdiary/utils.sh
+else printf '\033[1;31m/opt/sleepdiary/utils.sh not found - some checks bypassed.\033[0m\n'
+fi
+
+case "$1" in
+
+    build)
+        npm ci --silent && npx vue-cli-service build --dest docs --no-clean
+        ;;
+
+    test)
+        npx vue-cli-service lint
+        generic_tests
+        exit "$WARNED"
+        ;;
+
+    serve)
+        npm ci && npx vue-cli-service serve
+        ;;
+
+    upgrade)
+        npm upgrade
+        ;;
+
+    *)
+        exit 2
+        ;;
+
+esac
